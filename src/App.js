48f29e01c2289c5b74891be6e9a9d76736690264
psarson <psarson@gravoc.com>
2019-04-09 18:36:42 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react' 
+import Table from './components/Table'
 
-class App extends Component {
-  render() {
+export default class App extends React.Component { 
+  render() { 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={{ width: 'max-content' }}>
+        <Table x={4} y={4} id={'1'} />
       </div>
-    );
+    )
   }
 }
-
-export default App;
